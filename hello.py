d694ffd960151ec9cb6127e908aac14ebc45643d
Levy Nunes <levy.vix@gmail.com>
2023-03-17 16:28:11 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,37 @@
+from prefect import flow, task
+import polars as pl
+
+
+@task
+def read_data(path) -> pl.DataFrame:
+    return pl.scan_csv(path)
+
+
+@task
+def preprocess(data) -> pl.DataFrame:
+    # add 10 years to every age
+    return data.with_columns([
+        pl.col("age") + 15,
+        pl.col('name').str.to_lowercase().str.contains('a').alias('has_a'),
+    ]).with_columns([
+        (pl.col('age') % 4 == 0).alias('age_div_4'),
+    ])
+
+
+@task(log_prints=True)
+def write_data(data: pl.DataFrame) -> None:
+    result = data.collect()
+    print(result.limit(3))
+    result.write_parquet("data.parquet")
+
+@flow(name="Polars Data Processing")
+def main() -> None:
+    data = read_data("data.csv")
+
+    preprocesed_data = preprocess(data)
+
+    write_data(preprocesed_data)
+
+
+if __name__ == "__main__":
+    main()
